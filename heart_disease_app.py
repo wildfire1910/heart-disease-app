3420ddd200780645f7b7e08a385e60762828814a
wildfire1910 <abhiramferrari4@gmail.com>
2025-03-20 12:58:38 -0700

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -165,7 +165,7 @@ def main():
     # Input fields for user data
     age = st.number_input("Age", min_value=0, max_value=120, value=predefined_data["age"])
     sex = st.selectbox("Sex", options=[0, 1], format_func=lambda x: "Female" if x == 0 else "Male", index=predefined_data["sex"])
-    cp = st.selectbox("Chest Pain Type (cp)", options=[0, 1, 2, 3], index=predefined_data["cp"])
+    cp = st.selectbox("Chest Pain Type (cp)", options=[0, 1, 2, 3], index=predefined_data["cp"]-1)
     trestbps = st.number_input("Resting Blood Pressure (trestbps)", min_value=0, max_value=200, value=predefined_data["trestbps"])
     chol = st.number_input("Serum Cholesterol (chol)", min_value=0, max_value=600, value=predefined_data["chol"])
     fbs = st.selectbox("Fasting Blood Sugar > 120 mg/dl (fbs)", options=[0, 1], index=predefined_data["fbs"])
@@ -173,7 +173,7 @@ def main():
     thalach = st.number_input("Maximum Heart Rate Achieved (thalach)", min_value=0, max_value=220, value=predefined_data["thalach"])
     exang = st.selectbox("Exercise Induced Angina (exang)", options=[0, 1], index=predefined_data["exang"])
     oldpeak = st.number_input("ST Depression Induced by Exercise (oldpeak)", min_value=0.0, max_value=10.0, value=predefined_data["oldpeak"])
-    slope = st.selectbox("Slope of the Peak Exercise ST Segment (slope)", options=[0, 1, 2], index=predefined_data["slope"])
+    slope = st.selectbox("Slope of the Peak Exercise ST Segment (slope)", options=[0, 1, 2], index=predefined_data["slope"]-1)
     ca = st.number_input("Number of Major Vessels Colored by Fluoroscopy (ca)", min_value=0, max_value=4, value=predefined_data["ca"])
     thal = st.selectbox("Thalassemia (thal)", options=[1, 2, 3, 4, 5, 6, 7], index=predefined_data["thal"] - 1)
 
